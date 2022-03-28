b2d80c4c42e2edbb53df235a648dff43a4674d77
Phili23 <rmariacarmen78@gmail.com>
2022-03-28 16:24:26 -0500

    Creando modelo Movieâ

---
+++ api/src/models/Movie.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("movie", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //preguntar por el unique
+    },
+    adult: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    //backdrop_path
+    img: {
+      type: DataTypes.STRING,
+    },
+    //Overview
+    Overview: {
+      type: DataTypes.STRING,
+    },
+    popularity: {
+      type: DataTypes.FLOAT,
+    },
+    release_date: {
+      type: DataTypes.STRING,
+    },
+    spoken_languages: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    vote_average: {
+      type: DataTypes.FLOAT,
+    },
+  });
+};
