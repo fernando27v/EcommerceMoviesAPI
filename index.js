768055f55dda4e8f51e861fc14423b1a57fcf4a8
fernando27v <fernandovale3004@gmail.com>
2022-03-29 16:34:32 -0300

    Change of index.js from api to source

--- index.js
+++ index.js
@@ -17,8 +17,8 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require('./api/src/app.js');
+const { conn } = require('./api/src/db.js');
 
 // Syncing all the models at once.
 conn.sync({force:false}).then(() => {
