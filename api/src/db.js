63ba2abf028a6771c9986f6292840b1f532436e3
fernando27v <fernandovale3004@gmail.com>
2022-03-29 14:48:47 -0300

    Fix app.js and db.js

--- api/src/db.js
+++ api/src/db.js
@@ -28,7 +28,7 @@ process.env.NODE_ENV === 'production'
     ssl:true
 })
 : new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@:${DB_HOST}/ecommerce`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
