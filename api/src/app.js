890605844ab0d94f4a91e1e805221875ef385d06
fernando27v <fernandovale3004@gmail.com>
2022-03-29 13:32:53 -0300

    Ready for deploy

---
+++ api/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
+
+
+const server = express();
+const cors = require('cors')
+server.name = 'API';
+
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
+server.use(bodyParser.json({ limit: '100mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+server.use('/', routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
