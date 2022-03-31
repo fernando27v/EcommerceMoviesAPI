fe8f6d19b7bafc36a84b52fa2741b2d4b51e062c
usuario08 <aamc5991@gmail,com>
2022-03-31 16:55:04 -0500

    Load Data

--- api/src/middlewares/loadData.js
+++ api/src/middlewares/loadData.js
@@ -7,8 +7,8 @@ async function loadGenres() {
   } = await axios.get(
     "https://api.themoviedb.org/3/genre/movie/list?api_key=4ec209a2aac7d23aba9a83b713409fab&language=en-US"
   );
-  const crear = [...genres].forEach(async(e) => {
-    await Genre.findOrCreate({ where: { id: e.id } });
+  const crear = [...genres].forEach(async (e) => {
+    await Genre.findOrCreate({ where: { id: e.id, name: e.name } });
   });
 }
 module.exports = { loadGenres };
