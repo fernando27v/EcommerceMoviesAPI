64e9fa8f9268d784fe98e177904da56e435549b4
fernando27v <fernandovale3004@gmail.com>
2022-04-01 17:42:11 -0300

    Creation routes shopping cart and functions

---
+++ api/src/routes/shopping_cart.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const shoppingCartRouter = express.Router();
+const getItems = require("../controllers/shopping_cart/getItems");
+const clearItems = require("../controllers/shopping_cart/clearItems");
+const postItems = require("../controllers/shopping_cart/postItems");
+
+shoppingCartRouter.get("/:id",getItems);
+shoppingCartRouter.delete("/:id",clearItems);
+shoppingCartRouter.post("/:id",postItems);
+
+module.exports = {shoppingCartRouter};
