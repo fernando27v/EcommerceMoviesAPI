78c5db9dfb253f4ac68e13185157f6046afbea48
usuario08 <aamc5991@gmail,com>
2022-03-31 21:01:41 -0500

    cambio index.js false

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 //const {cargaMovies}=require('./src/carga/cargaMovies')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, async () => {
     //wait cargaMovies();
     console.log(`% listening at ${process.env.PORT}`); // eslint-disable-line no-console
