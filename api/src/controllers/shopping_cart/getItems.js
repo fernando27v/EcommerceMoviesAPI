9eafb165db5e775ea409ec52adc9c64daa33fb99
usuario08 <aamc5991@gmail,com>
2022-04-04 12:53:20 -0500

    get shopping_cart add

--- api/src/controllers/shopping_cart/getItems.js
+++ api/src/controllers/shopping_cart/getItems.js
@@ -1,6 +1,15 @@
-module.exports ={
+const { shopping_cart } = require("../../db");
 
-
-    getItems :() =>{}
-    
+module.exports = {
+  getItems: async ({ params: { id } }, res) => {
+    try {
+      const cart = await shopping_cart.findAll({
+        where: { UserUid: id },
+        include: [{ model: Movie, attributes: ["id", "title", "price"] }],
+      });
+      res.json(cart);
+    } catch (error) {
+      res.json({ error });
     }
+  },
+};
