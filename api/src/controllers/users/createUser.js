99a63924ab3502ea2c04db720cad1e6245ceebaf
fernando27v <fernandovale3004@gmail.com>
2022-04-14 22:53:09 -0300

    Change in route Create user

--- api/src/controllers/users/createUser.js
+++ api/src/controllers/users/createUser.js
@@ -1,5 +1,4 @@
-const { Op } = require("sequelize");
-const bcryptjs = require("bcryptjs");
+
 const { User } = require("../../db.js");
 
 
@@ -9,29 +8,25 @@ const createUser = async (req, res) => {
 
   try {
     //Valido si el email o el username existen en la DB
-    let user = await User.findOne({
-      where: { email },
-    });
-
-    //Si el email o el username existen retorna esto
-    if (user) {
-      return res.status(400).json({
-        msg: "Already exists an account with that email or username",
-      });
-    } else {
-
-      //El usuario no existe, procedo a crear el usuario
-      user = await User.build({
+    const user = await User.findOne({where:{email}});
+    if(user){
+      return res.status(200).json(user);
+    }else{
+      let user = await User.findOrCreate({
+      where: { 
         email,
         nickname,
         name,
         lastName
-      })
-      await user.save();
-
-      return res.status(200).json(user);
-    
+       },
+    });
+    return res.status(200).json(user);
     }
+    
+
+      
+    
+    
   } catch (error) {
     console.error(error);
     return 0;
